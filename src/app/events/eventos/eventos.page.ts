a166b40d46d0bfd7b512967f16380a8569002516
fernovg <fer800.fv@gmail.com>
2025-07-01 02:46:54 -0600

    diseño eventos

--- src/app/events/eventos/eventos.page.ts
+++ src/app/events/eventos/eventos.page.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol, IonRow, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonItem, IonIcon, IonLabel, IonChip } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addCircle, calendar, camera, people, qrCode, qrCodeOutline, location } from 'ionicons/icons';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.page.html',
   styleUrls: ['./eventos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonChip, IonLabel, IonIcon, IonItem, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonRow, IonCol, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class EventosPage implements OnInit {
 
-  constructor() { }
-
+  constructor() {
+    addIcons({ addCircle, qrCodeOutline, calendar, location, people, camera, qrCode });
+  }
   ngOnInit() {
   }
 
