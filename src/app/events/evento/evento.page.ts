9990ec6f47601224d764dade9ad43c7d451d3f09
fernovg <fer800.fv@gmail.com>
2025-07-01 18:18:46 -0600

    diseño evento

--- src/app/events/evento/evento.page.ts
+++ src/app/events/evento/evento.page.ts
@@ -1,20 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonActionSheet, IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonIcon, IonSegment, IonSegmentButton, IonSegmentContent, IonSegmentView, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonAvatar, IonItem, IonGrid, IonRow, IonCol, IonList, IonLabel } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addCircle, albums, calendar, camera, ellipsisHorizontal, grid, heart, people, qrCodeOutline, settings, ellipsisHorizontalOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-evento',
   templateUrl: './evento.page.html',
   styleUrls: ['./evento.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonLabel, IonItem, IonAvatar, IonList, IonCol, IonRow, IonGrid, IonActionSheet, IonCardTitle, IonCardHeader, IonCard, IonSegmentButton, IonSegment, IonIcon, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, IonSegmentContent, IonSegmentView, CommonModule, FormsModule]
 })
 export class EventoPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({qrCodeOutline,settings,albums,grid,heart,ellipsisHorizontalOutline,addCircle,people,camera,ellipsisHorizontal});
+  }
 
   ngOnInit() {
   }
 
+  isActionSheetOpen = false;
+
+  public actionSheetButtons = [
+    {
+      text: 'Guardar',
+      data: {
+        action: 'save',
+      },
+    },
+    {
+      text: 'Borrar',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Compartir',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ]
+
+  setOpen(isOpen: boolean) {
+    this.isActionSheetOpen = isOpen;
+  }
+
+  clic(){
+    console.log('clic')
+  }
 }
--- src/app/events/eventos/eventos.page.ts
+++ src/app/events/eventos/eventos.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol, IonRow, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonItem, IonIcon, IonLabel, IonChip } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol, IonRow, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonItem, IonIcon, IonLabel, IonChip, IonButton } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { addCircle, calendar, camera, people, qrCode, qrCodeOutline, location } from 'ionicons/icons';
 
@@ -10,13 +10,14 @@ import { addCircle, calendar, camera, people, qrCode, qrCodeOutline, location } 
   templateUrl: './eventos.page.html',
   styleUrls: ['./eventos.page.scss'],
   standalone: true,
-  imports: [IonChip, IonLabel, IonIcon, IonItem, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonRow, IonCol, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonChip, IonLabel, IonIcon, IonItem, IonCardTitle, IonCardHeader, IonCardContent, IonCard, IonRow, IonCol, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class EventosPage implements OnInit {
 
   constructor() {
     addIcons({ addCircle, qrCodeOutline, calendar, location, people, camera, qrCode });
   }
+  
   ngOnInit() {
   }
 
