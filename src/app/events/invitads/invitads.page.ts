8280f1908479ed31ce143f2ad39228dcf1f0f5f6
fernovg <fer800.fv@gmail.com>
2025-07-10 02:40:46 -0600

    diseño perfil v1 invitados v1

--- src/app/events/invitads/invitads.page.ts
+++ src/app/events/invitads/invitads.page.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonIcon, IonActionSheet, IonItemSliding, IonAvatar, IonItemOption, IonList, IonItem, IonItemOptions, IonLabel } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { add, pin, share, trash } from 'ionicons/icons';
 
 @Component({
   selector: 'app-invitads',
   templateUrl: './invitads.page.html',
   styleUrls: ['./invitads.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonLabel, IonItemOptions, IonItem, IonList, IonItemOption, IonAvatar, IonItemSliding, IonIcon, IonButton, IonContent, CommonModule, FormsModule, IonActionSheet]
 })
 export class InvitadsPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({add,pin,share,trash});
+  }
 
   ngOnInit() {
   }
+
+  public actionSheetButtons = [
+    {
+      text: 'Nombre',
+      // role: 'destructive'
+    },
+    {
+      text: 'Fotos'
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
 
 }
--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonCard, IonAvatar, IonList, IonItem, IonSelectOption, IonSelect, IonLabel, IonChip, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { cogOutline, createOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonButton, IonChip, IonLabel, IonItem, IonList, IonAvatar, IonCard, IonContent, CommonModule, FormsModule, IonSelect, IonSelectOption]
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({createOutline,cogOutline});
+  }
 
   ngOnInit() {
   }
